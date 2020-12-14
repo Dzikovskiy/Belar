b457aa0788f2f7d29fc8f506437be10895a1e3d8
Vitali <vital.dz@mail.ru>
2020-12-15 00:53:21 +0300

    register page check
    register page check

--- app/src/main/java/com/example/vitaliy/belor/RegisterActivity.java
+++ app/src/main/java/com/example/vitaliy/belor/RegisterActivity.java
@@ -1,14 +1,68 @@
 package com.example.vitaliy.belor;
 
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editTextEmail, editTextPassword, editTextName;
+    private Button buttonRegister;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        buttonRegister = findViewById(R.id.buttonRegistrationRegister);
+        editTextEmail = findViewById(R.id.editTextRegisterEmailAddress);
+        editTextPassword = findViewById(R.id.editTextRegisterPassword);
+        editTextName = findViewById(R.id.editTextRegisterName);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+
     }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String name = editTextName.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (name.isEmpty()) {
+            editTextName.setError("Name is required");
+            editTextName.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            editTextPassword.setError("Password is less than 6 characters");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Email is wrong");
+            editTextEmail.requestFocus();
+            return;
+        }
+    }
+
+
 }
