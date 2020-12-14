f01fceca1d66069baa6752fa8ee7551e4022595d
Vitali <vital.dz@mail.ru>
2020-12-15 00:22:32 +0300

    registration
    registration

--- app/src/main/java/com/example/vitaliy/belor/LoginActivity.java
+++ app/src/main/java/com/example/vitaliy/belor/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.vitaliy.belor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button buttonRegistration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        buttonRegistration = findViewById(R.id.buttonRegister);
+        buttonRegistration.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),RegisterActivity.class);
+                startActivity(intent);
+               // finish();
+            }
+        });
     }
 }
